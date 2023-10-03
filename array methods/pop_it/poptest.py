d3b6f207a54a4aa6d589652a987dfaaf77522c8c
Joseph Macharia <sejokarizz@gmail.com>
2023-10-03 10:09:23 +0000

    add test to check if remove was used.

--- array methods/pop_it/poptest.py
+++ array methods/pop_it/poptest.py
@@ -1,4 +1,6 @@
 import unittest
+import inspect
+
 from pop import can_you_pop_it  # Change 'custom_pop' to 'pop'
 
 class TestCanYouPopIt(unittest.TestCase):
@@ -19,11 +21,16 @@ class TestCanYouPopIt(unittest.TestCase):
         self.assertEqual(popped_item, 5)
         self.assertEqual(my_list, [1, 2, 3, 4])
 
-    @unittest.expectedFailure
+    def test_remove_is_tricky(self):
+        my_list = [1, 2, 5, 3, 4, 5]
+        popped_item = can_you_pop_it(my_list)
+        self.assertEqual(popped_item, 5)
+        self.assertEqual(my_list, [1, 2, 5, 3, 4])
+
+
     def test_prevent_builtin_pop(self):
-        empty_list = []
-        with self.assertRaises(IndexError):
-            empty_list.pop()
+        source_code = inspect.getsource(can_you_pop_it)
+        self.assertNotIn('.pop', source_code)
 
 if __name__ == '__main__':
     unittest.main()
